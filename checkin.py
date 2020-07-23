87bc6c7bf241552eff0176c6cd07a54d422a3161
914813666 <914813666@qq.com>
2020-07-23 18:19:24 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -1,39 +1,28 @@
-import requests,json,os
-
-# server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
-sever = os.environ["SERVE"]
-# 填写server酱sckey,不开启server酱则不用填
-sckey = os.environ["SCKEY"]
-#'SCU89402Tf98b7f01ca3394b9ce9aa5e2ed1abbae5e6ca42796bb9'
-# 填入glados账号对应cookie
-cookie = os.environ["COOKIE"]
-#'__cfduid=d3459ec306384ca67a65170f8e2a5bd561593049467; _ga=GA1.2.766373509.1593049472; _gid=GA1.2.1338236108.1593049472; koa:sess=eyJ1c2VySWQiOjQxODMwLCJfZXhwaXJlIjoxNjE4OTY5NTI4MzY4LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=6qG8SyMh_5KpSB6LBc9yRviaPvI'
-
-
-
+# -*- coding: utf8 -*-
+# -*- coding: utf8 -*-
+import requests,os
+import re
+import time
+from urllib.parse import quote
 def start():
-    
-    url= "https://glados.rocks/api/user/checkin"
-    url2= "https://glados.rocks/api/user/status"
-    referer = 'https://glados.rocks/console/checkin'
-    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
-    state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
-   # print(res)
-
-    if 'message' in checkin.text:
-        mess = checkin.json()['message']
-        time = state.json()['data']['leftDays']
-        time = time.split('.')[0]
-        #print(time)
-        if sever == 'on':
-            requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
-    else:
-        requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
-
+    try:
+        s = requests.session()
+        cookie =  os.environ["COOKIE"]
+        this_time = int(round(time.time() * 1000))
+        login_url = 'https://ikuuu.co/user/checkin'
+        headers={
+            'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+            'accept-encoding':'gzip, deflate, br',
+            'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language':'zh-CN,zh;q=0.9,en;q=0.8',
+            'Cookie': cookie
+        }
+        res =s.post(login_url,headers=headers).text
+        print('访问结果：'+res)
+    except Exception as e:
+        print("地址访问失败，通知SERVER酱！")
+        requests.get('https://sc.ftqq.com/填自己的，不填也没关系，不影响.send?text=' + quote('白嫖V2访问出现问题~'+time.strftime('%Y.%m.%d',time.localtime(time.time()))) +'&desp='+quote('异常代码：\n'+str(e)))
 def main_handler(event, context):
-  return start()
-
+    return start()
 if __name__ == '__main__':
     start()
-
-    
